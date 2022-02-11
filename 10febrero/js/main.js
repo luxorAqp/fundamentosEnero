964ce7962ec929677cbae5b63893b7a21ed31580
brendagq <brennnda.gonzalezq@gmail.com>
2022-02-10 21:59:52 -0600

    Funciones

--- 10febrero/js/main.js
+++ 10febrero/js/main.js
@@ -1,4 +1,4 @@
-/*
+
 const productos = [
     {
         imagenPrincipal: "https://picsum.photos/id/0/200/300",
@@ -35,9 +35,9 @@ const productos = [
         imagenFondo:"https://picsum.photos/id/674/200/300",
         nombre: "Kilo de uva",
         precio: "1500",
-    }
+    },
 ]
-*/
+
 
 
 /*
@@ -53,31 +53,137 @@ const productos = [
 */
 const section = document.getElementsByTagName("section")[0]
 
-const card = document.createElement( "div" )
-card.classList.add( "card" )
 
-const imagesWrapper = document.createElement("div")
-imagesWrapper.classList.add( "img-wrapper" )
+function crearCard( productoInfo ) {    
+    const card = document.createElement( "div" )
+    card.classList.add( "card" )
 
-const firstImg = document.createElement( "img" )
-firstImg.src = "https://picsum.photos/200/300"
-firstImg.classList.add("principal-img")
+    const imagesWrapper = document.createElement("div")
+    imagesWrapper.classList.add( "img-wrapper" )
 
-const secondImg = document.createElement("img")
-secondImg.src = "https://picsum.photos/id/175/200/300"
+    const firstImg = document.createElement( "img" )
+    firstImg.src = productoInfo.imagenPrincipal
+    firstImg.classList.add("principal-img")
 
-const paragraph = document.createElement("p")
-paragraph.textContent = "Nombre del producto"
+    const secondImg = document.createElement("img")
+    secondImg.src = productoInfo.imagenFondo
 
-const button = document.createElement("button")
-button.textContent = "Comprar"
+    const paragraph = document.createElement("p")
+    paragraph.textContent = productoInfo.nombre
 
-card.appendChild( imagesWrapper )
-card.appendChild( paragraph )
-card.appendChild( button )
+    const button = document.createElement("button")
+    button.textContent = "Comprar"
 
-imagesWrapper.appendChild( firstImg )
-imagesWrapper.appendChild( secondImg )
+    card.appendChild( imagesWrapper )
+    card.appendChild( paragraph )
+    card.appendChild( button )
 
-section.appendChild( card )
-console.log( card )
+    imagesWrapper.appendChild( firstImg )
+    imagesWrapper.appendChild( secondImg )
+
+    return card
+}
+
+let fragmento = new DocumentFragment()
+
+for( let i = 0; i < productos.length; i++ ){
+
+    let card = crearCard( productos[i] )
+
+    fragmento.appendChild( card )
+}
+
+section.appendChild( fragmento )
+
+
+
+//FUNCIONES
+
+
+//Funciones declaradas
+/*
+function nombreDeLaFunción( parametros ){
+    //instrucciones
+    return valorDeRetorno
+}
+
+nombreDeLaFunción( argumentos )
+*/
+
+
+//Funciones expresadas, expresión de funciones, funciones anonimas
+/*
+const variableQueGuardaLaFuncion = function(parametros){
+    //instrucciones
+    return valorDeRetorno
+}
+
+variableQueGuardaLaFuncion( argumentos )
+*/
+
+
+suma( 5, 10 )
+
+function suma( num1, num2 ){
+    console.log( num1 + num2 )
+}
+
+suma( 2, 2 )
+
+
+//resta( 50, 10 )
+
+const resta = function(num1, num2){
+    return num1 - num2
+}
+
+resta( 10, 5 )
+
+
+//Función flecha
+const funcionFlecha = ( parametro1, parametro2 ) => {
+    //instrucciones
+    return valorDeRetorno
+}
+const funcionFlechaConUnParametro = parametro => {
+    //instrucciones
+    return valorDeRetorno
+}
+
+const funcionFlechaReturnImplicito = parametro => valorDeRetorno
+
+const funcionFlechaSinParametro = () => valorDeRetorno
+
+const restarDosNumeros = ( num1, num2 ) => num1 - num2
+
+
+//Funciones de orden superior
+//-Reciben otras funciones como parametros
+//-Devuelven otra función como valor de retorno 
+
+
+
+function saludarUsuario( nombre ){
+    console.log(`Hola ${nombre}! buenas noches.`)
+}
+
+
+function convertirAMayusculas (nombre){
+    console.log( nombre.toUpperCase() )
+}
+
+
+//Funcion de orden superior
+function obtenerNombre( callback ){ //Se ejecuta primero
+    let nombre = prompt( "Ingresa es tu nombre" )
+
+    callback( nombre ) // saludarUsuario(nombre)
+}
+
+obtenerNombre( saludarUsuario )
+
+obtenerNombre( convertirAMayusculas )
+
+
+//Callbacks
+//Closures
