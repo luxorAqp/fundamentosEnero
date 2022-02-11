266fd50ee4b4c50a443184a5cf7b7a6930c23733
brendagq <brennnda.gonzalezq@gmail.com>
2022-02-10 19:14:32 -0600

    Carpeta 10 de febrero. Arreglo de productos

---
+++ 10febrero/js/main.js
@@ -0,0 +1,40 @@
+/*
+const productos = [
+    {
+        imagenPrincipal: "https://picsum.photos/id/0/200/300",
+        imagenFondo:"https://picsum.photos/id/1/200/300",
+        nombre: "Computadora portatil",
+        precio: "1500",
+    },
+    {
+        imagenPrincipal: "https://picsum.photos/id/1062/200/300",
+        imagenFondo:"https://picsum.photos/id/1025/200/300",
+        nombre: "Cobija para perro",
+        precio: "1500",
+    },
+    {
+        imagenPrincipal: "https://picsum.photos/id/111/200/300",
+        imagenFondo:"https://picsum.photos/id/133/200/300",
+        nombre: "Automovil",
+        precio: "1500",
+    },
+    {
+        imagenPrincipal: "https://picsum.photos/id/250/200/300",
+        imagenFondo:"https://picsum.photos/id/319/200/300",
+        nombre: "Camara",
+        precio: "1500",
+    },
+    {
+        imagenPrincipal: "https://picsum.photos/id/30/200/300",
+        imagenFondo:"https://picsum.photos/id/326/200/300",
+        nombre: "Taza",
+        precio: "1500",
+    },
+    {
+        imagenPrincipal: "https://picsum.photos/id/75/200/300",
+        imagenFondo:"https://picsum.photos/id/674/200/300",
+        nombre: "Kilo de uva",
+        precio: "1500",
+    }
+]
+*/
